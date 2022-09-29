8203ca529fcc3d7d23c7f8df058f3e93e44b8ea2
Student509s <Student509@tumo.world>
2022-09-29 17:51:30 +0200

    final project

--- gameoflife finished/script(2).js
+++ gameoflife finished/script(2).js
@@ -1358,28 +1358,28 @@ function Acidrainstarts(){
 
 
 }
-function main() {
-    var socket = io();
-    var chatDiv = document.getElementById('Chat');
-    var input = document.getElementById('Nachricht');
-    var button = document.getElementById('Senden');
+// function main() {
+//     var socket = io();
+//     var chatDiv = document.getElementById('Chat');
+//     var input = document.getElementById('Nachricht');
+//     var button = document.getElementById('Senden');
  
-    function handleSubmit(evt) {
-        var val = input.value;
-        if (val != "") {
-            socket.emit("send message", val);
-        }
-    }
-    button.onclick = handleSubmit;
+//     function handleSubmit(evt) {
+//         var val = input.value;
+//         if (val != "") {
+//             socket.emit("send message", val);
+//         }
+//     }
+//     button.onclick = handleSubmit;
  
-    function handleMessage(msg) {
-        var p = document.createElement('p');
-        p.innerText = msg;
-        chatDiv.appendChild(p);
-        input.value = "";
-}
+//     function handleMessage(msg) {
+//         var p = document.createElement('p');
+//         p.innerText = msg;
+//         chatDiv.appendChild(p);
+//         input.value = "";
+// }
 
-socket.on('display message', handleMessage);
-} // main closing bracket
+// socket.on('display message', handleMessage);
+// } // main closing bracket
 
-window.onload = main;   
+// window.onload = main;   
